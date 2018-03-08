f9cadc3e2444a74d9575aa628dfc38466bc9ddf4
laura <laura.canelada.velasco@alumnos.upm.es>
2018-03-08 20:40:49 +0100

    actualizacion 08/03/2018

--- Control.java
+++ Control.java
@@ -14,13 +14,11 @@ while (seguir){
 pieza = aplicacion.getNextToken();
 	
 switch (pieza.kind) {
+
 case TextoConstants.patron1:  { System.out.println(pieza.image + "\nAprobada la asignatura\n"); break;}	//Aprobada la asignatura
 case TextoConstants.patron2:  { System.out.println(pieza.image + "\nAprobada la teoría y suspensa la práctica\n"); break;}	//Aprobada la teoria y suspensa la practica
 case TextoConstants.patron3:  { System.out.println(pieza.image + "\nAprobada la práctica y suspensa la teoría\n"); break;} //Aprobada la practica y suspensa la teoria
 case TextoConstants.patron4:  { System.out.println(pieza.image + "\nSuspensa la teoría y suspensa la práctica\n"); break;} //Suspensa la teoria y suspensa la practica
-case TextoConstants.patron5:  { System.out.println("Error léxico, falta el comentario"); break;}
-case TextoConstants.patron6:  { System.out.println("Error léxico, falta el comentario"); break;}	
-case TextoConstants.patron7:  { System.out.println("Error léxico, falta el comentario"); break;}
 	
 }
 seguir = pieza.kind != TextoConstants.EOF;
